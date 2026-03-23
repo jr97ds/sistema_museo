bb661cb21bf7cd3116810fcdd86919f5224987a4
jorojas84 <jorojas84@unisalle.edu.co>
2026-03-23 09:48:57 -0500

    correcion de visibilidad ciertas clases

--- usuarios.py
+++ usuarios.py
@@ -3,38 +3,40 @@ from abc import ABC
 
 class UsuarioRegistrado(ABC):
     def __init__(self, nombre, apellido, usuario, contraseña):
-        self.nombre = nombre
-        self.apellido = apellido
-        self.usuario = usuario
-        self.contraseña = contraseña
+        self._nombre = nombre
+        self._apellido = apellido
+        self._usuario = usuario
+        self._contraseña = contraseña
 
     def mostrar_nombre_completo(self) -> None:
-        print(f"Nombre: {self.nombre} {self.apellido}")
+        print(f"Nombre: {self._nombre} {self._apellido}")
 
 class Empleado(UsuarioRegistrado):
     def __init__(self, nombre : str, apellido : str, 
                  usuario : str, contraseña : str, cargo : str):
         super().__init__(nombre, apellido, usuario, contraseña)
-        self.cargo = cargo
-    
+        self._cargo = cargo
 
 class EncargadoCatalogo(UsuarioRegistrado):
-    def __init__(self, nombre : str, apellido : str, usuario : str, contraseña : str, departamento : str):
+    def __init__(self, nombre : str, apellido : str, 
+                 usuario : str, contraseña : str):
         super().__init__(nombre, apellido, usuario, contraseña)
 
 
 class RestauradorJefe(UsuarioRegistrado):
-    def __init__(self, nombre : str, apellido : str, usuario : str, contraseña : str, especialidad : str):
+    def __init__(self, nombre : str, apellido : str, 
+                 usuario : str, contraseña : str):
         super().__init__(nombre, apellido, usuario, contraseña)
 
 class DirectorMuseo(UsuarioRegistrado):
-    def __init__(self, nombre, apellido, usuario, contraseña, departamento):
+    def __init__(self, nombre : str, apellido : str, 
+                 usuario : str, contraseña : str):
         super().__init__(nombre, apellido, usuario, contraseña)
 
 class Autenticacion:
     def __init__(self, usuario, contraseña):
-        self.usuario = usuario
-        self.contraseña = contraseña
+        self._usuario = usuario
+        self._contraseña = contraseña
 
 
 
