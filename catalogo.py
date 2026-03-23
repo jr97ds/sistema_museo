21c7c3791c67b8e36aa20f72d427eaf1d37d2f1b
jorojas84 <jorojas84@unisalle.edu.co>
2026-03-23 09:22:02 -0500

    clases catalogo, salas y pantalla

---
+++ catalogo.py
@@ -0,0 +1,12 @@
+class Catalogo:
+    def __init__(self, obras : list):
+        self._obras = obras
+
+class Salas:
+    def __init__(self, nombre : str, obras : list):
+        self._nombre = nombre
+        self._obras = obras
+
+class Pantalla: 
+    def __init__(self,):
+        pass
