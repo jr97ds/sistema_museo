77a3177c14a6d9548a40861961951740890c8e0e
jorojas84 <jorojas84@unisalle.edu.co>
2026-03-24 07:55:52 -0500

    creacion de menu por tpo de empleado

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from datetime import date
 import Auth
+from menus import MenuEncargadoCatalogo, MenuRestauradorJefe
 from obras import Cuadro, Escultura, Otro
 from catalogo import Catalogo, Pantalla, Sala
 from tramites import MuseoExterno
@@ -53,101 +54,11 @@ def mostrar_menu_principal() -> None:
     print("2. Sistema de Empleados")
     print("0. Salir")
 
-def mostrar_menu_encargado_catalogo(catalogo: Catalogo, salas: list[Sala]) -> None:
-        while True:
-                    print("\n¿Qué desea hacer?")
-                    print("1. Ver catálogo")
-                    print("2. Agregar obra al catálogo")
-                    print("0. Cerrar sesión")
-                    opcion_empleado_catalogo = seleccion_opcion()
-                    # Mostrar Catalogo
-                    if opcion_empleado_catalogo == "1":
-                        catalogo.mostrar_obras()
-                        continue
-                    # Agregar Producto
-                    elif opcion_empleado_catalogo == "2":
-                        nueva_obra = crear_producto()
-                        catalogo.agregar_obra(nueva_obra)
-                        print("\nObra agregada al catálogo:")
-                        print(nueva_obra)
-                        continue 
-                    # Cerrar Sesión de Empleado
-                    elif opcion_empleado_catalogo == "0":
-                        Auth.logout(): # type: ignore
-                        print("\nSesión cerrada.")
-                        break
-                    else:
-                        no_valido()
+        
 
 def mostrar_menu_restaurador_jefe(empleado: RestauradorJefe, catalogo: Catalogo) -> None:
     while True:
-                print("\n¿Qué desea hacer?")
-                print("1. Ver obras en exhibición")
-                print("2. Ver restauraciónes")
-                print("3. Enviar obra a restauración")
-                print("4. Finalizar restauración")
-                print("0. Cerrar sesión")
-                opcion_empleado_restauracion = seleccion_opcion()
-                # Mostrar Obras en Exhibición
-                if opcion_empleado_restauracion == "1":
-                    catalogo.mostrar_obras()
-                    continue
-                # Mostrar Restauraciones
-                elif opcion_empleado_restauracion == "2":
-                    mostrar_restauraciones(catalogo)
-                    continue
-                # Enviar obra a restauración
-                elif opcion_empleado_restauracion == "3":
-                    while True:
-                        titulo_obra = input(
-                            "\nIngrese el título de la obra a restaurar: "
-                            )
-                        obra_a_restaurar = catalogo.buscar_obra(titulo_obra)
-                    
-                        if obra_a_restaurar:
-                            # Si ya esta en restauración, no se puede enviar de nuevo
-                            if obra_a_restaurar.estado == "En restauración":
-                                print(f"\nLa obra '{obra_a_restaurar.titulo}' ya está en restauración.")
-                                break
-                            # Ruta para enviar obra a restuarar
-                            else:
-                                tipo_restauracion = input(
-                                "Ingrese el tipo de restauración: "
-                                )
-                                empleado.enviar_a_restauracion(obra_a_restaurar, #type: ignore
-                                                                    tipo_restauracion) 
-                                print(f"\nObra '{obra_a_restaurar.titulo}' enviada a restauración.")
-                                break
-                        else:
-                            print(f"\nObra '{titulo_obra}' no encontrada en exhibición.")
-                            continue
-                        # Marcar obra como restaurada
-                elif opcion_empleado_restauracion == "4":
-                    while True:
-                        titulo_obra = input(
-                            "\nIngrese el título de la obra a finalizar: "
-                            )
-                        obra_a_finalizar = catalogo.buscar_obra(titulo_obra)
-                        
-                        if obra_a_finalizar:
-                            if obra_a_finalizar.estado == "En restauración":
-                                empleado.finalizar_restauracion(obra_a_finalizar) # type: ignore
-                                print(f"\nLa restauracion de la obra '{obra_a_finalizar.titulo}' fue finalizada.")
-                                break
-                            else:
-                                print(f"\nLa obra '{obra_a_finalizar.titulo}' no está en restauración.")
-                                break
-                        else:
-                                print(f"\nObra '{titulo_obra}' no encontrada")
-                                continue  
-                        
-                elif opcion_empleado_restauracion == "0":
-                    Auth.logout()
-                    print ("\nSesión cerrada.")
-                    break   
-                else:
-                    no_valido()
-                    continue
+                
 
 def mostrar_menu_director_museo(empleado: DirectorMuseo, catalogo: Catalogo, museos_externos: list[MuseoExterno]) -> None:
       while True:
@@ -197,95 +108,10 @@ def mostrar_menu_director_museo(empleado: DirectorMuseo, catalogo: Catalogo, mus
                     no_valido()
                     continue
 
-def no_valido() -> None:
-    print("\nOpción no válida. Por favor, elija una opción válida.")
-    
-def seleccion_opcion() -> str:
-    opcion = input("\nSeleccione una opción: ")
-    return opcion
-
-def asignar_sala(salas) -> Sala:
-    for idx, sala in enumerate(salas):
-        print(f"{idx + 1}. {sala.nombre}")
-    while True:
-        opcion_sala = int(input("\nSeleccione la sala para la obra: "))
-        if 1 <= opcion_sala <= len(salas):
-            sala_seleccionada = salas[opcion_sala - 1]
-            return sala_seleccionada
-        else:
-            print("\nOpción de sala no válida.")
-            continue
-
 def crear_producto():
-    while True:
-        print("\n¿Qué tipo de obra desea agregar?")
-        print("1. Cuadro")
-        print("2. Escultura")   
-        print("3. Otro")
-        tipo_obra = seleccion_opcion()
-
-        if tipo_obra == "1":
-            nombre = input("Nombre del cuadro: ")
-            autor = input("Autor del cuadro: ")
-            periodo = input("Periodo del cuadro: ")
-            valoracion = int(input("Valor: "))
-            fecha_creacion = date.fromisoformat(input(
-                "Fecha de creación (YYYY-MM-DD): "))
-            fecha_entrada = date.today() # Fecha actual
-            tecnica = input("Técnica del cuadro: ")
-            estilo = input("Estilo del cuadro: ")
-            sala = asignar_sala(salas)
-            nuevo_cuadro = Cuadro(nombre, autor, periodo, valoracion, 
-                                fecha_creacion, fecha_entrada, tecnica, estilo)
-            sala.agregar_obra(nuevo_cuadro)
-            return nuevo_cuadro
-    
-        elif tipo_obra == "2":
-            nombre = input("Nombre de la escultura: ")
-            autor = input("Autor de la escultura: ")
-            periodo = input("Periodo de la escultura: ")
-            valoracion = int(input("Valor: "))
-            fecha_creacion = date.fromisoformat(input(
-                "Fecha de creación (YYYY-MM-DD): "))
-            fecha_entrada = date.today() # Fecha actual
-            material = input("Material de la escultura: ")
-            estilo = input("Estilo de la escultura: ")
-            sala = asignar_sala(salas)
-            nueva_escultura = Escultura(nombre, autor, periodo, valoracion, 
-                                        fecha_creacion, fecha_entrada, 
-                                        material, estilo)
-            sala.agregar_obra(nueva_escultura)
-            return nueva_escultura
-        
-        elif tipo_obra == "3":
-            nombre = input("Nombre de la obra: ")
-            autor = input("Autor de la obra: ")
-            periodo = input("Periodo de la obra: ")
-            valoracion = int(input("Valor: "))
-            fecha_creacion = date.fromisoformat(input(
-                "Fecha de creación (YYYY-MM-DD): "))
-            fecha_entrada = date.today() # Fecha actual
-            sala = asignar_sala(salas)
-            nueva_obra = Otro(nombre, autor, periodo, valoracion, 
-                            fecha_creacion, fecha_entrada)
-            sala.agregar_obra(nueva_obra)
-            return nueva_obra
-        else:
-            no_valido()
-            continue
-
+  
 def mostrar_restauraciones(catalogo: Catalogo) -> None:
-        organizadas = []
-        for obra in catalogo.obras:
-            for restauracion in obra.restauraciones:
-                organizadas.append(restauracion)
 
-        if organizadas:
-            organizadas.sort(key=lambda r: r.fecha_inicio)
-            for restauracion in organizadas:
-                print(restauracion)
-        else:
-            print("\nNo hay obras en restauración.")
 
 def mostrar_cesiones(catalogo: Catalogo) -> None:
         encontradas = False
@@ -354,15 +180,17 @@ while True:
                 
 
         # Ruta para encargado del catalogo
-        if empleado_actual.cargo == "Encargado del Catálogo": #type: ignore
-            mostrar_menu_encargado_catalogo(catalogo, salas)
+        if Auth.empleado_actual.cargo == "Encargado del Catálogo": 
+            menu = MenuEncargadoCatalogo(Auth.empleado_actual, catalogo=catalogo, salas=salas)
+            menu.ejecutar()
 
         # Ruta para restaurador jefe
-        elif empleado_actual.cargo == "Restaurador Jefe": # type: ignore
-              mostrar_menu_restaurador_jefe(empleado_actual, catalogo) # type: ignore
+        elif Auth.empleado_actual.cargo == "Restaurador Jefe": # type: ignore
+            menu = MenuRestauradorJefe(Auth.empleado_actual, catalogo=catalogo) 
+            menu.ejecutar()
 
         elif empleado_actual.cargo == "Director del Museo": # type: ignore
-             mostrar_menu_director_museo(empleado_actual, catalogo, museos_externos) # type: ignore
+             
            
         
     elif opcion == "0":
--- menus.py
+++ menus.py
@@ -1,11 +1,13 @@
 
 from abc import ABC, abstractmethod
-
+from datetime import date
+from typing import Optional
 from catalogo import Catalogo, Sala
-from obras import Obra
-from main import crear_producto
-from usuarios import Empleado, EncargadoCatalogo
-from auth import Auth
+from obras import Cuadro, Escultura, Obra, Otro
+from tramites import MuseoExterno
+from usuarios import Empleado
+from usuarios import DirectorMuseo, EncargadoCatalogo, RestauradorJefe
+
 
 def seleccion_opcion() -> str:
     opcion = input("\nSeleccione una opción: ")
@@ -14,53 +16,297 @@ def seleccion_opcion() -> str:
 def no_valido() -> None:
     print("\nOpción no válida. Por favor, elija una opción válida.")
 
+def asignar_sala(salas) -> Sala:
+    for idx, sala in enumerate(salas):
+        print(f"{idx + 1}. {sala.nombre}")
+    while True:
+        opcion_sala = int(input("\nSeleccione la sala para la obra: "))
+        if 1 <= opcion_sala <= len(salas):
+            sala_seleccionada = salas[opcion_sala - 1]
+            return sala_seleccionada
+        else:
+            print("\nOpción de sala no válida.")
+            continue
 
 class Menu(ABC):
     """Clase base para representar un menú en el sistema del museo."""
     def __init__(self, empleado: Empleado, **contexto):
-        self.empleado = empleado
-        self.contexto = contexto
+        self._empleado = empleado
+        self._contexto = contexto
     
     @abstractmethod
     def ejecutar(self) -> None:
         """Método abstracto para ejecutar loop del menu"""
         pass
 
-    @abstractmethod
-    def crear_menu(self,empleado: Empleado, **kwargs) -> None:
-        """Método abstracto para crear el menú específico 
-        según el tipo de empleado"""
+    @staticmethod
+    def crear_menu(empleado: Empleado, **kwargs) -> None:
+        """Crea el menú adecuado según el tipo de empleado"""
+
         if isinstance(empleado, EncargadoCatalogo):
-            return MenuEncargadoCatalogo(empleado, **kwargs)
+            return MenuEncargadoCatalogo(empleado, **kwargs) #type: ignore
         elif isinstance(empleado, RestauradorJefe):
-            return MenuRestauradorJefe(empleado, **kwargs)
+            return MenuRestauradorJefe(empleado, **kwargs) #type: ignore
         elif isinstance(empleado, DirectorMuseo):
-            return MenuDirectorMuseo(empleado, **kwargs)
+            return MenuDirectorMuseo(empleado, **kwargs) #type: ignore  
         raise ValueError("Tipo de empleado no reconocido para crear menú.")
     
-    class MenuEncargadoCatalogo(Menu):
-    
-    while True:
-        print("\n¿Qué desea hacer?")
-        print("1. Ver catálogo")
-        print("2. Agregar obra al catálogo")
-        print("0. Cerrar sesión")
-        opcion_empleado_catalogo = seleccion_opcion()
-        # Mostrar Catalogo
-        if opcion_empleado_catalogo == "1":
-            catalogo.mostrar_obras()
-            continue
-        # Agregar Producto
-        elif opcion_empleado_catalogo == "2":
-            nueva_obra = crear_producto()
+class MenuEncargadoCatalogo(Menu):
+    """Clase para representar el menú del encargado del catálogo"""
+
+    def ejecutar(self) -> None:
+        catalogo = self._contexto["catalogo"]
+        salas = self._contexto["salas"]
+        while True:
+            print("\n¿Qué desea hacer?")
+            print("1. Ver catálogo")
+            print("2. Agregar obra al catálogo")
+            print("0. Cerrar sesión")
+            opcion_empleado_catalogo = seleccion_opcion()
+            # Mostrar Catalogo
+            if opcion_empleado_catalogo == "1":
+                catalogo.mostrar_obras()
+            # Agregar Producto
+            elif opcion_empleado_catalogo == "2":
+                nueva_obra = self._crear_obra(catalogo, salas)
+                print("\nObra agregada al catálogo:")
+                print(nueva_obra) 
+            # Cerrar Sesión de Empleado
+            elif opcion_empleado_catalogo == "0":
+                break
+            else:
+                no_valido()
+
+    def _crear_obra(self, catalogo: Catalogo,
+                     salas: list[Sala]) -> Optional[Obra]:
+        """Crear una nueva obra a partir de la entrada del usuario"""
+        while True:
+            print("\n¿Qué tipo de obra desea agregar?")
+            print("1. Cuadro")
+            print("2. Escultura")   
+            print("3. Otro")
+            tipo_obra = seleccion_opcion()
+            nombre = input("Nombre del cuadro: ")
+            autor = input("Autor del cuadro: ")
+            periodo = input("Periodo del cuadro: ")
+
+            try:
+                valor = int(input("Valor: "))
+                fecha_creacion = date.fromisoformat(input(
+                    "Fecha de creación (YYYY-MM-DD): "))
+            except ValueError: 
+                print("\nValor o fecha no válidos. " \
+                "Por favor, ingrese un número para el valor y " \
+                "una fecha en formato YYYY-MM-DD.")
+                return None
+
+            fecha_entrada = date.today() # Fecha actual
+
+
+            if tipo_obra == "1":
+                tecnica = input("Técnica del cuadro: ")
+                estilo = input("Estilo del cuadro: ")
+                nueva_obra = Cuadro(nombre, autor, periodo, valor, 
+                                    fecha_creacion, fecha_entrada, 
+                                    tecnica, estilo)
+                       
+            elif tipo_obra == "2":
+                material = input("Material de la escultura: ")
+                estilo = input("Estilo de la escultura: ")
+                nueva_obra = Escultura(nombre, autor, periodo, valor, 
+                                            fecha_creacion, fecha_entrada, 
+                                            material, estilo)
+            
+            elif tipo_obra == "3":
+                nueva_obra = Otro(nombre, autor, periodo, valor, 
+                                fecha_creacion, fecha_entrada)
+            else:
+                no_valido()
+
+            sala = asignar_sala(salas)
+            sala.agregar_obra(nueva_obra)
             catalogo.agregar_obra(nueva_obra)
-            print("\nObra agregada al catálogo:")
-            print(nueva_obra)
-            continue 
-        # Cerrar Sesión de Empleado
-        elif opcion_empleado_catalogo == "0":
-            Auth.logout(): # type: ignore
-            print("\nSesión cerrada.")
-            break
+            return nueva_obra
+
+
+class MenuRestauradorJefe(Menu):
+    """Clase para representar el menú del restaurador jefe"""
+    def ejecutar(self) -> None:
+        empleado = self._empleado
+        catalogo = self._contexto["catalogo"]
+
+        while True:
+            print("\n¿Qué desea hacer?")
+            print("1. Ver obras en exhibición")
+            print("2. Ver restauraciónes")
+            print("3. Enviar obra a restauración")
+            print("4. Finalizar restauración")
+            print("0. Cerrar sesión")
+            opcion_empleado_restauracion = seleccion_opcion()
+            # Mostrar Obras en Exhibición
+            if opcion_empleado_restauracion == "1":
+                catalogo.mostrar_obras()
+            # Mostrar Restauraciones
+            elif opcion_empleado_restauracion == "2":
+                self._mostrar_restauraciones(catalogo)
+            # Enviar obra a restauración
+            elif opcion_empleado_restauracion == "3":
+                self._enviar_a_restauracion(self._empleado, # type: ignore
+                                            catalogo) 
+            # Marcar obra como restaurada
+            elif opcion_empleado_restauracion == "4":
+                self._finalizar_restauracion(self._empleado, # type: ignore
+                                             catalogo) 
+                    
+            elif opcion_empleado_restauracion == "0":
+                break   
+            else:
+                no_valido()
+
+    @staticmethod
+    def _mostrar_restauraciones(catalogo: Catalogo) -> None:
+        organizadas = []
+        for obra in catalogo.obras:
+            for restauracion in obra.restauraciones:
+                organizadas.append(restauracion)
+
+        if organizadas:
+            organizadas.sort(key=lambda r: r.fecha_inicio)
+            for restauracion in organizadas:
+                print(restauracion)
         else:
-            no_valido()
+            print("\nNo hay obras en restauración.")
+
+    @staticmethod
+    def _enviar_a_restauracion(empleado: RestauradorJefe, 
+                               catalogo: Catalogo) -> None:
+        """Ruta para enviar obra a restaurar"""
+
+        titulo_obra = input("\nIngrese el título de la obra a restaurar: ")
+        obra_a_restaurar = catalogo.buscar_obra(titulo_obra)
+    
+        if not obra_a_restaurar:
+            print(f"\nObra '{titulo_obra}' no encontrada en exhibición.")
+            return
+        if obra_a_restaurar.estado == "En restauración":
+            print(f"\nLa obra '{obra_a_restaurar.titulo}'"
+                    f"ya está en restauración.")
+            return
+        tipo_restauracion = input("Ingrese el tipo de restauración: ")
+        empleado.enviar_a_restauracion(obra_a_restaurar,
+                                        tipo_restauracion) 
+        print(f"\nObra '{obra_a_restaurar.titulo}' enviada a restauración.")
+
+    @staticmethod
+    def _finalizar_restauracion(empleado: RestauradorJefe, 
+                                catalogo: Catalogo) -> None:
+        """Ruta para finalizar restauración de obra"""
+
+        titulo_obra = input(
+            "\nIngrese el título de la obra a finalizar: "
+            )
+        obra_a_finalizar = catalogo.buscar_obra(titulo_obra)
+        
+        if obra_a_finalizar:
+            if obra_a_finalizar.estado == "En restauración":
+                empleado.finalizar_restauracion(obra_a_finalizar)
+                print(f"\nLa restauracion de la obra "
+                      f"'{obra_a_finalizar.titulo}' fue finalizada.")
+                return
+            else:
+                print(f"\nLa obra '{obra_a_finalizar.titulo}'"
+                      f" no está en restauración.")
+                return
+        else:
+                print(f"\nObra '{titulo_obra}' no encontrada")
+                return  
+
+
+class MenuDirectorMuseo(Menu):
+    """Clase para representar el menú del director del museo"""
+    def ejecutar(self) -> None:
+        empleado = self._empleado
+        catalogo = self._contexto["catalogo"]
+        museos_externos = self._contexto["museos_externos"]
+        
+        while True:
+            print("\n¿Qué desea hacer?")
+            print("1. Ver valor total del catálogo")
+            print("2. Ver todas las cesiones")
+            print("3. Crear nueva cesión")
+            print("0. Cerrar sesión")
+            opcion_empleado_director = seleccion_opcion()
+            
+            # Mostrar valor total del catálogo
+            if opcion_empleado_director == "1":
+                valor_total = catalogo.calcular_valor_total()
+                print(f"\nValor total del catálogo: ${valor_total}")
+                
+            # Ver todas las cesiones
+            elif opcion_empleado_director == "2":
+                self._mostrar_cesiones(catalogo)
+                
+            #Crear nueva cesion
+            elif opcion_empleado_director == "3":
+                self._crear_cesion(self._empleado, catalogo, # type: ignore
+                                    museos_externos) 
+
+            elif opcion_empleado_director == "0":
+                break
+            else:
+                no_valido()
+
+
+    @staticmethod               
+    def _mostrar_cesiones(catalogo: Catalogo) -> None:
+        """Muestra todas las cesiones del catálogo 
+        ordenadas por fecha de inicio"""
+        encontradas = False
+        for obra in catalogo.obras:
+            if obra.cesiones:
+                encontradas = True
+                for cesion in obra.cesiones:
+                    print(cesion)
+        if not encontradas:
+            print("\nNo hay obras en cesión.")
+    
+    @staticmethod
+    def _crear_cesion(empleado: DirectorMuseo, catalogo: Catalogo, 
+                      museos_externos: list[MuseoExterno]) -> None:
+        """Crea una nueva cesión para una obra del catálogo"""
+
+        input_titulo = input(
+            "\nIngrese el título de la obra para la cesión: "
+            )
+        obra = catalogo.buscar_obra(input_titulo)
+        if not obra:
+            print(f"\nObra '{input_titulo}' no encontrada.")
+            return
+        input_museo = input(
+            "\nIngrese el nombre del museo externo para la cesión: "
+            )
+        museo = None
+        for m in museos_externos:
+            if m.nombre.lower() == input_museo.lower():
+                museo = m
+                break
+        if not museo:
+            print(f"\nMuseo externo '{input_museo}' no encontrado.")
+            return
+
+
+        try:
+            importe = int(input("\nIngrese el importe de la cesión: "))
+            duracion_dias = int(
+                input("\nIngrese la duración de la cesión en días: ")
+                )
+        except ValueError:
+            print("\nValores invalidos. Por favor, ingrese números enteros.")
+            return
+        
+        cesion = empleado.crear_cesion(obra, museo, importe, duracion_dias) 
+        print(f"\nSolicitud de Cesión creada: {cesion}")
+        print(f"\nEstado: {cesion.estado}")
+        
+        
+       
