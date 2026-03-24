5ffc0d88b17c99a4a2c6d43bcfc4a1e0ea900336
jorojas84 <jorojas84@unisalle.edu.co>
2026-03-23 21:45:33 -0500

    correcion de estilo y funcionalidad de getters y setters en obras

--- obras.py
+++ obras.py
@@ -1,29 +1,39 @@
-from abc import ABC
+from abc import ABC, abstractmethod
 from datetime import date
-from catalogo import Sala
 
 class Obra(ABC):
-    def __init__(self, nombre : str, autor : str, periodo : str,
-                  valor : int, fecha_creacion : date, 
-                  fecha_entrada : date, sala : Sala ):
+    """Clase base para representar una obra de arte en el museo."""
+    def __init__(self, nombre: str, autor: str, periodo: str,
+                  valor: int, fecha_creacion: date, 
+                  fecha_entrada: date):
         self._nombre = nombre
         self._autor = autor
         self._periodo = periodo
-        self._valor =  valor
+        self._valor = valor
         self._fecha_creacion = fecha_creacion
         self._fecha_entrada = fecha_entrada
         self._estado = "En exhibición"
         self._restauraciones = []
         self._cesiones = []
-        self._sala = sala
+
+    @abstractmethod
+    def detalles_especificos(self) -> str:
+        """Método abstracto para obtener detalles específicos de 
+        cada tipo de obra."""
+        pass
 
     def __str__(self) -> str:
-        return (
-        f"Nombre: {self._nombre}, Autor: {self._autor},"
-        f"Periodo: {self._periodo}, Valor: {self._valor},"
-        f"Estado: {self._estado} , Fecha de Creación: {self._fecha_creacion}, "
-        f"Fecha de Entrada: {self._fecha_entrada} , Sala: {self._sala.nombre}"
+        inicial = (
+        f"Nombre: {self._nombre}, Autor: {self._autor}, "
+        f"Periodo: {self._periodo}, Valor: {self._valor}, "
+        f"Estado: {self._estado}, Fecha de Creación: {self._fecha_creacion}, "
+        f"Fecha de Entrada: {self._fecha_entrada}"
         )
+        extras = self.detalles_especificos()
+        if extras:
+            return inicial + ", " + extras
+        else:
+            return inicial
     
     @property
     def restauraciones(self):
@@ -51,47 +61,54 @@ class Obra(ABC):
     
     @estado.setter
     def estado(self, nuevo_estado):
+        estados_validos = ["En exhibición", "En restauración", "En cesión"]
+        # Asegura que solo se puedan asignar estados válidos a la obra
+        if nuevo_estado not in estados_validos:
+            raise ValueError(
+                f"Estado no válido"
+                f"Opciones {estados_validos}"
+            )
+        
         self._estado = nuevo_estado
 
-    @property
-    def sala(self):
-        return self._sala   
 
 class Cuadro(Obra):
-    def __init__(self, nombre : str, autor : str, periodo : str,
-                 valor : int, fecha_creacion : date, 
-                 fecha_entrada : date, tecnica : str , 
-                 estilo : str , sala : Sala):
+    """Clase para representar un cuadro, hereda de Obra."""
+    def __init__(self, nombre: str, autor: str, periodo: str,
+                 valor: int, fecha_creacion: date, 
+                 fecha_entrada: date, tecnica: str, 
+                 estilo: str):
         super().__init__(nombre, autor, periodo, valor, 
-                         fecha_creacion, fecha_entrada, sala)
+                         fecha_creacion, fecha_entrada)
         self._tecnica = tecnica
         self._estilo = estilo
 
-    def __str__(self) -> str:
-        return (super().__str__() + 
-        f", Técnica: {self._tecnica},"
-        f"Estilo: {self._estilo}"
-        )
+    def detalles_especificos(self) -> str:
+        return f"Técnica: {self._tecnica}, Estilo: {self._estilo}"
+
 
 class Escultura(Obra):
-    def __init__(self, nombre : str, autor : str, periodo : str,
-                 valor : int, fecha_creacion : date, 
-                 fecha_entrada : date, material : str , 
-                 estilo : str , sala : Sala):
+    """Clase para representar una escultura, hereda de Obra."""
+    def __init__(self, nombre: str, autor: str, periodo: str,
+                 valor: int, fecha_creacion: date, 
+                 fecha_entrada: date, material: str, 
+                 estilo: str):
         super().__init__(nombre, autor, periodo, valor, 
-                         fecha_creacion, fecha_entrada, sala) 
+                         fecha_creacion, fecha_entrada)
         self._material = material
         self._estilo = estilo
 
-    def __str__(self) -> str:
-        return (super().__str__() + 
-        f", Material: {self._material},"
-        f"Estilo: {self._estilo}"
-        )
+    def detalles_especificos(self) -> str:
+        return f"Material: {self._material}, Estilo: {self._estilo}"
+
 
 class Otro(Obra):
-    def __init__(self, nombre : str, autor : str, periodo : str,
-                 valor : int, fecha_creacion : date, 
-                 fecha_entrada : date, sala : Sala):
+    """Clase para representar otro tipo de obra, hereda de Obra."""
+    def __init__(self, nombre: str, autor: str, periodo: str,
+                 valor: int, fecha_creacion: date, 
+                 fecha_entrada: date):
         super().__init__(nombre, autor, periodo, valor, 
-                         fecha_creacion, fecha_entrada, sala)
+                         fecha_creacion, fecha_entrada)
+
+    def detalles_especificos(self) -> str:
+        return ""
