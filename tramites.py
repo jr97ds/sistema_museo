802ebf6724302f5039e94345c2ab380d377aed8f
jorojas84 <jorojas84@unisalle.edu.co>
2026-03-23 21:46:12 -0500

    correcion de estilo y comprobacion de estados validos

--- tramites.py
+++ tramites.py
@@ -1,65 +1,35 @@
-from abc import ABC
-from datetime import date, datetime
+from abc import ABC, abstractmethod
+from datetime import date
 from obras import Obra
 
 class Tramite (ABC):
-    def __init__(self, obra : Obra):
+    """Clase base para representar un trámite relacionado con una obra"""
+    ESTADOS_VALIDOS = []
+
+    def __init__(self, obra: Obra):
         self._obra = obra
         self._fecha_inicio = None
         self._fecha_fin = None
         self._estado = ""
 
-class Restauracion(Tramite):
-    def __init__(self, obra : Obra, fecha_inicio : date, 
-                 tipo : str):
-        super().__init__(obra)
-        self._fecha_inicio = fecha_inicio
-        self._estado = "En proceso"
-        self._tipo = tipo  
-    
-    @property
-    def tipo(self):
-        return self._tipo
     @property
     def estado(self):
         return self._estado
+    
     @property
     def fecha_inicio(self):
         return self._fecha_inicio
-
-    def __str__(self) -> str:
-        return (f"Restauración de '{self._obra.titulo}' - " 
-                f"Tipo: {self._tipo} - " 
-                f"Estado: {self._estado} - Fecha Inicio: "
-                f"{self._fecha_inicio} - Fecha Fin: {self._fecha_fin}")
-
-class Cesion(Tramite):
-    def __init__(self, obra : Obra, duracion_dias : int, 
-                 museo_externo : MuseoExterno, importe : int):
-        super().__init__(obra)
-        self.duracion_dias = duracion_dias
-        self._estado = "Pendiente"
-        self._museo_externo = museo_externo
-        self._importe = importe
-    
-    @property
-    def museo_externo(self):
-        return self._museo_externo
-    
-    @property
-    def estado(self):
-        return self._estado
     
     @property
     def fecha_fin(self):
         return self._fecha_fin
-    
-    @property
-    def fecha_inicio(self):
-        return self._fecha_inicio
-    
+
     @estado.setter
     def estado(self,nuevo_estado):
+        # Asegura que solo se puedan asignar estados válidos al trámite
+        if nuevo_estado not in self.ESTADOS_VALIDOS:
+            raise ValueError(f"Estado no válido."
+                             f"Estados válidos: {self.ESTADOS_VALIDOS}")
         self._estado = nuevo_estado
 
     @fecha_fin.setter
@@ -70,17 +40,74 @@ class Cesion(Tramite):
     def fecha_inicio(self, nueva_fecha_inicio):
         self._fecha_inicio = nueva_fecha_inicio
     
+    # Método abstracto para añadir detalles específicos de cada tipo de trámite
+    @abstractmethod
+    def detalles_especificos(self) -> str:  
+        pass
+
     def __str__(self) -> str:
-        return (f"Cesión de '{self._obra.titulo}' - Museo: "
-                f"{self._museo_externo._nombre} - Importe: "
-                f"${self._importe} - Estado: {self._estado} - "
-                f"Fecha Inicio: {self._fecha_inicio} - Fecha Fin: "
-                f"{self._fecha_fin}")
+        inicial = (f"Obra: '{self._obra.titulo}' - "  
+                f"Estado: {self._estado} - Fecha Inicio: "
+                f"{self._fecha_inicio} - Fecha Fin: {self._fecha_fin}")
+        extras = self.detalles_especificos()
+        if extras:
+            return inicial + ", " + extras
+        else:
+            return inicial
+
+class Restauracion(Tramite):
+    """Clase para representar un trámite de restauración, hereda de Tramite."""
+    # Define los estados válidos para el trámite de restauración
+    ESTADOS_VALIDOS = ["En proceso", "Finalizada"]
+
+    def __init__(self, obra: Obra, fecha_inicio: date, 
+                 tipo: str):
+        super().__init__(obra)
+        self._fecha_inicio = fecha_inicio
+        self.estado = "En proceso"
+        self._tipo = tipo  
+    
+    @property
+    def tipo(self):
+        return self._tipo
+    
+    def detalles_especificos(self) -> str:
+        return f"Tipo de Restauración: {self._tipo}"
 
 class MuseoExterno():
-    def __init__(self, nombre : str):
+    """Clase para representar un museo al que se le puede ceder una obra."""
+    def __init__(self, nombre: str):
         self._nombre = nombre
+    
+    @property
+    def nombre(self):
+        return self._nombre
+    
 
-@property
-def nombre(self):
-    return self._nombre
+class Cesion(Tramite):
+    """Clase para representar un trámite de cesión, hereda de Tramite."""
+    # Define los estados válidos para el trámite de cesión
+    ESTADOS_VALIDOS = ["Pendiente", "Aprobada", "Finalizada"]
+
+    def __init__(self, obra: Obra, duracion_dias: int, 
+                 museo_externo: MuseoExterno, importe: int):
+        super().__init__(obra)
+        self._duracion_dias = duracion_dias
+        self.estado = "Pendiente"
+        self._museo_externo = museo_externo
+        self._importe = importe
+    
+    @property
+    def museo_externo(self):
+        return self._museo_externo
+    
+    @property
+    def importe(self):
+        return self._importe
+    
+    def detalles_especificos(self) -> str:
+        return (f"Museo: {self._museo_externo.nombre}, "
+                f"Duración (días): {self._duracion_dias}, "
+                f"Importe: {self._importe}")
+
+
